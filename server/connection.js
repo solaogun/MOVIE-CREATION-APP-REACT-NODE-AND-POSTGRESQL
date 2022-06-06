d5553f87a25a554ba00379001095f7fc273af184
Saidat Olaogun <solaogun@softalliance.co.ng>
2022-06-06 20:40:19 +0100

    adding heroku postgress credentials

--- server/connection.js
+++ server/connection.js
@@ -1,11 +1,16 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    host: "localhost",
-    user: "solaogun",
-    port: 5433,
-    password: "Saidat",
-    database: "movie"
+    host: "ec2-34-225-159-178.compute-1.amazonaws.com",
+    user: "jnkytizawwkvbc",
+    port: 5432,
+    password: "7771a4b2c92f4146fadd08ba6cdbc1bd6eb608a8afc2b9a762144358a6766139",
+    database: "dfg15ml3tesi9"
+    // host: "localhost",
+    // user: "solaogun",
+    // port: 5433,
+    // password: "Saidat",
+    // database: "movie"
 })
 
 client.connect()
