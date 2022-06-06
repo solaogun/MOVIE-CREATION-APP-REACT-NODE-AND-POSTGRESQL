f8b527acf1b37532b1fac8428b2c845471871156
Saidat Olaogun <solaogun@softalliance.co.ng>
2022-06-06 21:38:12 +0100

    ssl

--- server/connection.js
+++ server/connection.js
@@ -6,7 +6,12 @@ const client = new Client({
     // port: 5432,
     // password: "rdcAxLPwRJGNLjFY_z0ABugnQryKD-15",
     // database: "mzocvtoh",
-    connectionString: "postgres://uqlzmpqlwkebes:bbdb5b0f86207013e4936785ba86e29aa2c8697d0580e25d00570381801c8891@ec2-3-226-163-72.compute-1.amazonaws.com:5432/dhc9m4breqe0t?ssl=true"
+    connectionString: "postgres://uqlzmpqlwkebes:bbdb5b0f86207013e4936785ba86e29aa2c8697d0580e25d00570381801c8891@ec2-3-226-163-72.compute-1.amazonaws.com:5432/dhc9m4breqe0t?ssl=true",
+    ssl: {
+        rejectUnauthorized: false,
+        key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+        cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+      },
     // host: "localhost",
     // user: "solaogun",
     // port: 5433,
